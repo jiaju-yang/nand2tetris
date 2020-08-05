1d3cd5b11472c05cc8bc2adc20892175c4cb491a
jiaju-yang <jiaju.yang@outlook.com>
2020-08-05 22:37:10 +0800

    Add project04 'Mult' program implementation

--- projects/04/mult/mult.asm
+++ projects/04/mult/mult.asm
@@ -7,3 +7,38 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+// i=R1
+// mul=0
+// while i>0:
+//     mul=mul+R1
+//     i--
+// store mul in R0
+
+    @R1
+    D=M
+    @i
+    M=D
+    @mul
+    M=0
+(LOOP)
+    @i
+    D=M
+    @STORE
+    D;JEQ
+    @i
+    M=M-1
+    @R0
+    D=M
+    @mul
+    M=M+D
+    @LOOP
+    0;JMP
+
+(STORE)
+    @mul
+    D=M
+    @R2
+    M=D
+(END)
+    @END
+    0;JMP
